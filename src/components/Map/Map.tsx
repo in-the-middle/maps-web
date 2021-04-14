2c8a1c1234e8d88951a93dbea3c1b1ce686566f0
Ivan Kameniev <kamenev2122@gmail.com>
2021-04-14 20:17:04 +0200

    initial commit

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'components/Map/Map.css'
+
+function Map() {
+  return (
+    <div className="leaflet-container">
+      <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  )
+}
+
+export default Map
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Map from 'components/Map/Map'
+
+function Main() {
+  return <Map />
+}
+
+export default Main
